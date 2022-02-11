c25c332440c2fac81952f75eff51cb1b26fdecee
BAM1523 <99370313+BAM1523@users.noreply.github.com>
2022-02-11 00:33:29 -0500

    Delete catkin_ws directory

--- catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/bhuvan/catkin_ws/src/assg2_git_ws/AuE8230Spring22_SaiBhuvanPathakamuri/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
